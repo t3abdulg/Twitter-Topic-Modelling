a59627b13dc7f005123cc738283c8c77f62f44a4
Tawfeeq <t3abdulg@uwaterloo.ca>
2015-08-16 22:20:59 -0400

    Update twitter query.py

--- twitter query.py
+++ twitter query.py
@@ -34,8 +34,8 @@ def ModelTopics(init_topic):
     # Iterating throught array, removing links.
     for i in range(0,len(tweets)):
         #removing all links, because really its just gonna mess up topic modeling
-        tweets[i] = re.sub(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))', '', tweets[i])
-        tweets[i] = re.sub(r'(?i)\b((?:http?://\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))', '', tweets[i])
+        tweets[i] = re.sub(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?Â«Â»â€œâ€â€˜â€™]))', '', tweets[i])
+        tweets[i] = re.sub(r'(?i)\b((?:http?://\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?Â«Â»â€œâ€â€˜â€™]))', '', tweets[i])
         #removing all new-line characters and hashtags
         tweets[i] = tweets[i].replace("#","")
         tweets[i] = tweets[i].replace("\n","")
@@ -65,7 +65,7 @@ def ModelTopics(init_topic):
     lda = ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=10, update_every=1, 
                             chunksize=10000, passes=10)
     
-    # print the probabilistic model
+    # print the probabilities
     topics =lda.print_topics(10)
     
     for i in range(0,len(topics)):
